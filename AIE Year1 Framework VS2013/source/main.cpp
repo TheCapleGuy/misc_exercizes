1b9dfd887606e0b74c6831a1fc584c6859d53f1d
2intheBush <adrianocaple@gmail.com>
2014-11-12 12:33:53 -0800

    Strings: High Scores

---
+++ AIE Year1 Framework VS2013/source/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "AIE.h"
+#include <iostream>
+
+using namespace std;
+
+int main( int argc, char* argv[] )
+{	
+    Initialise(800, 600, false, "My Awesome Game");
+    SetBackgroundColour(SColour(0, 0, 0, 255));
+   
+	//Game Loop
+    do
+    {
+        ClearScreen();
+
+
+    } while(!FrameworkUpdate());
+
+    Shutdown();
+
+    return 0;
+}
